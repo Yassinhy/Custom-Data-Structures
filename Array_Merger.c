bc270654a27c914dfbd8c57c02dfcd16b82539cd
Yassin Hisham Mohmed <yassinhywork@gmail.com>
2025-02-03 21:52:51 +0200

    Update Array_Merger.c
    Added comments and example usage

--- Array_Merger.c
+++ Array_Merger.c
@@ -1,3 +1,59 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+void MergeArrays (int nums1[], int nums1Size, int nums2[], int nums2Size, int sum[]);
+
+// compile time constants
+#define first_array_size 7
+#define second_array_size 7
+
+int main(void)
+{
+    // initialize first array
+    int first_array[first_array_size] = {2,4,7,12,13,15,18};
+
+    //print the first array
+    printf("First array:");
+    for (int i = 0; i < first_array_size; i++)
+    {
+        printf(" %d", first_array[i]);
+    }
+    printf("\n");
+
+
+    // initialize second array
+    int second_array[second_array_size] = {1,3,6,9,10,12,14};
+
+    //print the second array
+    printf("Second array:");
+    for (int i = 0; i < second_array_size; i++)
+    {
+        printf(" %d", second_array[i]);
+    }
+    printf("\n");
+
+    //initialize merged array
+    int merged_array_size = first_array_size + second_array_size;
+    int merged_array[merged_array_size];
+
+    //merge arrays
+    MergeArrays (first_array, first_array_size, second_array, second_array_size, merged_array);
+
+    //print the second array
+    printf("Merged array:");
+    for (int i = 0; i < merged_array_size; i++)
+    {
+        printf(" %d", merged_array[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+
 void MergeArrays (int nums1[], int nums1Size, int nums2[], int nums2Size, int sum[])
 {
     int i = 0, j = 0, k = 0;
